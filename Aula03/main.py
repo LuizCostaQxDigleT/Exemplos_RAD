0f3f1c05111f2aa4a50a7f82dac4b2db1f38dd39
LuizCostaQxDigleT <lpcosta3011@gmail.com>
2026-05-25 03:39:23 -0300

    3

---
+++ Aula03/main.py
@@ -0,0 +1,11 @@
+#Atributos do Objeto
+
+texto = open('C:/Users/luizpaulo/Documents/RAD/aula03/texto.txt')
+print("Nome do Arquivo: ", texto.name)
+print("Tamanho do arquivo posição atual(em bytes): ", texto.tell())
+print("Modo do arquvio: ", texto.mode)
+print("O arquvio está fechado? ", texto.closed)
+
+texto.close()
+
+print("O arquivo está fechado? ", texto.closed)
---
+++ Aula03/readline.py
@@ -0,0 +1,9 @@
+#Readline
+
+caminho_arquivo = 'C:/Users/luizpaulo/Documents/RAD/aula03/nomes.txt'
+
+arquivo = open(caminho_arquivo, 'r')
+linhas = arquivo.readlines()
+for i, linha in enumerate(linhas, start = 1):
+    print(f'Linha {i} - {linha}')
+arquivo.close()
